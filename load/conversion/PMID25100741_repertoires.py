3d826fc4a629bdb9ea7b6cc642429ee94153826a
Scott Christley <scott.christley@utsouthwestern.edu>
2019-11-08 16:07:01 -0600

    add PMID25100741

--- load/conversion/PMID25100741_repertoires.py
+++ load/conversion/PMID25100741_repertoires.py
@@ -1,5 +1,5 @@
 #
-# Conversion script for PRJNA248475
+# Conversion script for PMID25100741 (PRJNA248475)
 #
 # First run the standard convert_repertoires.py
 # Then run this to do specific cleanup
@@ -7,7 +7,8 @@
 
 import airr
 
-rep_file = './PRJNA248475.airr.json'
+rep_file = './repertoires.airr.json'
+out_file = './PMID25100741.airr.json'
 
 data = airr.load_repertoire(rep_file)
 reps = data['Repertoire']
@@ -24,13 +25,22 @@ for r in reps:
     r['sample'][0]['physical_linkage'] = 'none'
     r['sample'][0]['pcr_target'][0]['pcr_target_locus'] = 'IGH'
     r['subject']['organism']['id'] = "9606"
-    r['sample'][0]['cell_subset'] = null
+    r['sample'][0]['cell_subset'] = None
 
     a = r['subject']['age'].split(' ')
     r['subject']['age_min'] = int(a[0])
     r['subject']['age_max'] = int(a[0])
     r['subject']['age_unit']['id'] = 'UO_0000036'
     r['subject']['age_unit']['value'] = 'year'
+    del r['subject']['age']
+    r['subject']['diagnosis'] = None
+
+    if r['subject']['sex'] == 'Male':
+        r['subject']['sex'] = 'male'
+    if r['subject']['sex'] == 'Female':
+        r['subject']['sex'] = 'female'
+    r['data_processing'][0]['data_processing_id'] = '72775635-b44e-41ac-8acc-a6d92dac052c-007'
+
     #airr.schema.RepertoireSchema.validate_object(r)
 
-airr.write_repertoire(rep_file, reps)
+airr.write_repertoire(out_file, reps)
