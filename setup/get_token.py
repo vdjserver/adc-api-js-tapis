5909a04c0488313e4f93855ce8b388deba3932a9
Scott Christley <scott.christley@utsouthwestern.edu>
2019-11-12 11:15:58 -0600

    setup script

---
+++ setup/get_token.py
@@ -0,0 +1,47 @@
+#
+
+import json
+from dotenv import load_dotenv
+import os
+import airr
+import yaml
+import requests
+import argparse
+
+# Setup
+def getConfig():
+    if load_dotenv(dotenv_path='/api-js-tapis/.env'):
+        cfg = {}
+        cfg['api_server'] = os.getenv('WSO2_HOST')
+        cfg['api_key'] = os.getenv('WSO2_CLIENT_KEY')
+        cfg['api_secret'] = os.getenv('WSO2_CLIENT_SECRET')
+        cfg['username'] = os.getenv('VDJ_SERVICE_ACCOUNT')
+        cfg['password'] = os.getenv('VDJ_SERVICE_ACCOUNT_SECRET')
+        return cfg
+    else:
+        print('ERROR: loading config')
+        return None
+
+# Fetches a user token based on the supplied auth object
+# and returns the auth object with token data on success
+def getToken(config):
+    data = {
+        "grant_type":"password",
+        "scope":"PRODUCTION",
+        "username":config['username'],
+        "password":config['password']
+    }
+    headers = {
+        "Content-Type":"application/x-www-form-urlencoded"
+    }
+
+    url = 'https://' + config['api_server'] + '/token'
+
+    resp = requests.post(url, data=data, headers=headers, auth=(config['api_key'], config['api_secret']))
+    token = resp.json()
+    return token
+
+config = getConfig()
+print(config)
+token = getToken(config)
+print(token['access_token'])
