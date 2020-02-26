e23378a20585f2b9a162d56e34e91848278dbc1e
Scott Christley <scott.christley@utsouthwestern.edu>
2020-02-26 16:54:26 -0600

    AIRR schema changes

---
+++ app/load/conversion/cmv_repertoires.py
@@ -0,0 +1,25 @@
+#
+# Conversion script for emerson-2017-natgen
+# Adaptive Biotechnologies dataset
+#
+# Incorporate changes to the AIRR schema
+#
+
+import airr
+
+rep_file = './cmv.airr.json'
+#out_file = './cmv.airr.json'
+out_file = './test.airr.json'
+
+data = airr.load_repertoire(rep_file)
+reps = data['Repertoire']
+
+for r in reps:
+    files = r['data_processing'][0]['final_rearrangement_file'].split(',')
+    r['data_processing'][0]['data_processing_files'] = files
+    del r['data_processing'][0]['final_rearrangement_file']
+
+    for entry in r['sample']:
+        del entry['read_length']
+
+airr.write_repertoire(out_file, reps)
