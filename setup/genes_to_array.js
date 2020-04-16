c8f31e4345ddd741f9ff31440c2160b734647639
Scott Christley <scott.christley@utsouthwestern.edu>
2020-04-16 17:54:17 -0500

    rearrangement update

---
+++ setup/genes_to_array.js
@@ -0,0 +1,33 @@
+//
+// change gene calls to an array when it contains
+//
+
+//var cursor = db.rearrangement.find({repertoire_id:"1841923116114776551-242ac11c-0001-012"});
+var cursor = db.rearrangement.find({repertoire_id:"7578528411412459030-242ac116-0001-012"});
+
+var cnt = 0;
+var upcnt = 0;
+while ( cursor.hasNext() ) {
+    var obj = cursor.next();
+    //printjson(obj['_id']);
+    //printjson(obj['v_call']);
+    //printjson(typeof obj['v_call']);
+
+    if ((typeof obj['v_call']) == 'string') {
+	var fields = obj['v_call'].split(',');
+	if (fields.length > 1) {
+	    //printjson(fields);
+	    db.rearrangement.update({_id: obj._id},{$set:{"v_call":fields}});
+	    upcnt += 1;
+	}
+    }
+
+    cnt += 1;
+    if ((cnt % 10000) == 0) {
+	printjson(cnt);
+    }
+}
+printjson("Total");
+printjson(cnt);
+printjson("Updated");
+printjson(upcnt);
