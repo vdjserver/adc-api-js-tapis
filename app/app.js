2ea100a524aa85c50e8afd1ba93a03c6ea0bdfcc
Scott Christley <scott.christley@utsouthwestern.edu>
2020-05-01 12:37:51 -0500

    change default port

--- app/app.js
+++ app/app.js
@@ -1,5 +1,31 @@
 'use strict';
 
+//
+// app.js
+// Application entry point
+//
+// VDJServer Community Data Portal
+// ADC API for VDJServer
+// https://vdjserver.org
+//
+// Copyright (C) 2020 The University of Texas Southwestern Medical Center
+//
+// Author: Scott Christley <scott.christley@utsouthwestern.edu>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
 var app = require('express')();
 var path = require('path');
 var fs = require('fs');
@@ -48,7 +74,7 @@ GuestAccount.getToken()
 	    var swaggerExpress = runner.expressMiddleware();
 	    swaggerExpress.register(app);
 
-	    var port = config.port || 8080;
+	    var port = config.port || 8020;
 	    app.listen(port);
 
 	    console.log('VDJServer ADC API listening on port:' + port);
